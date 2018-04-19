0a1abd7c26627c56b6d5a80ab0910c30ccb7f050
Simon Koelsch <simon.koelsch@innoq.com>
2018-04-19 12:55:07 +0200

    added genesis block as constant

--- src/bullshit_chain/main.clj
+++ src/bullshit_chain/main.clj
@@ -1,10 +1,12 @@
 (ns bullshit-chain.main 
   (:require [digest :refer [sha-256]]))
   
-(defn to-json [block] "123")
+(def genesis "{\"index\":1,\"timestamp\":0,\"proof\":1917336,\"transactions\":[{\"id\":\"b3c973e2-db05-4eb5-9668-3e81c7389a6d\",\"timestamp\":0,\"payload\":\"I am Heribert Innoq\"}],\"previousBlockHash\":\"0\"}")
+
+(defn to-json [block] block)
 
 (defn to-hash [block]
   (sha-256 (to-json block)))
     
 (defn -main [& args]
-  (println (to-hash nil)))
+  (println (to-hash genesis)))
