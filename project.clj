5983c199f13d4b27197aafab9b478e505eb5c0bf
Michael Vitz <michael.vitz@innoq.com>
2018-04-19 12:57:12 +0200

    Add dependency for JSON conversion

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject bullshit-chain "0.0.1-SNAPSHOT"
   :min-lein-version "2.8.1"
   :dependencies [[digest "1.4.8"]
-                 [org.clojure/clojure "1.9.0"]]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :main bullshit-chain.main
   :uberjar-name "bullshit-chain.jar"
   :profiles {:uberjar {:aot [bullshit-chain.main]}})
