e11e5710ad0b24a0cfb0af4760f639eb1265a472
Simon Koelsch <simon.koelsch@innoq.com>
2018-04-19 12:26:11 +0200

    hallo hash

--- src/bullshit_chain/main.clj
+++ src/bullshit_chain/main.clj
@@ -1,5 +1,10 @@
-(ns bullshit-chain.main)
-
-(defn -main [& args]
-  (println "hello blockchain"))
+(ns bullshit-chain.main 
+  (:require [digest :refer [sha-256]]))
   
+(defn to-json [block] "123")
+
+(defn to-hash [block]
+  (sha-256 (to-json block)))
+    
+(defn -main [& args]
+  (println (to-hash nil)))
