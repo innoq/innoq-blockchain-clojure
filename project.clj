e2e26b6e6303b3105ae9bc90de1f0aa0b46077bf
Michael Vitz <michael.vitz@innoq.com>
2018-04-19 12:16:36 +0200

    Add dependency for hashing

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject bullshit-chain "0.0.1-SNAPSHOT"
   :min-lein-version "2.8.1"
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[digest "1.4.8"]
+                 [org.clojure/clojure "1.9.0"]]
   :main bullshit-chain.main
   :uberjar-name "bullshit-chain.jar"
   :profiles {:uberjar {:aot [bullshit-chain.main]}})
